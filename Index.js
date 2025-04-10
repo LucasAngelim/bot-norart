ce4efe44783a8c167d5bc1a7ff13282f6dc9a9bb
Lucas Angelim Paes <lucasangelim2@gmail.com>
2025-04-10 03:47:40 -0300

    Atualiza Index.js e prepara para subir pro Heroku

--- Index.js
+++ Index.js
@@ -4,6 +4,7 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+      executablePath: process.env.CHROME_BIN,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
   });
